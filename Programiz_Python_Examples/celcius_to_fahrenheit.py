623838e569dc5f0efb7d42497dfd4df85edef70f
Farah Rubena <82815385+farah-rubena@users.noreply.github.com>
2023-06-06 18:35:19 +0530

    Add files via upload

---
+++ Programiz_Python_Examples/celcius_to_fahrenheit.py
@@ -0,0 +1,7 @@
+'''WAPP to convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+'''
+
+celsius = float(input("Enter the temperature: "))
+cel_to_fahrenheit = (celsius * 9/5) + 32
+print(cel_to_fahrenheit)
